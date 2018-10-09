0fbbbdffc6c7016c98467e53b6502b109bc9e9e1
Hoa Vo <whoait@gmail.com>
2018-10-09 11:22:25 +0700

    Change code

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,18 @@
-//This component handles the App template used on every page.
+// This component handles the App template used on every page.
 import React, {PropTypes} from 'react';
+import {connect} from 'react-redux';
 
-class App extends React.Component{
+class App extends React.Component {
   render() {
-    return(
+    return (
       <div className="container-fluid">
-        <p>Header here...</p>
         {this.props.children}
       </div>
     );
   }
 }
 
-App.PropTypes = {
+App.propTypes = {
   children: PropTypes.object.isRequired
 };
 
--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import {Link} from 'react-router';
 
-class Homepage extends React.Component {
+class HomePage extends React.Component {
   render() {
-    return(
+    return (
       <div className="jumbotron">
-        <h1>Hoavv Admin</h1>
-        <p>React, Redux and React router in ES6 </p>
-        <Link to="about" className= "btn btn-primary btn-lg">Learn more</Link>
+        <h1>Pluralsight Administration</h1>
+        <p>React, Redux and React Router in ES6 for ultra-responsive web apps.</p>
+        <Link to="about" className="btn btn-primary btn-lg">Learn more</Link>
       </div>
     );
   }
 }
 
-export default Homepage;
+export default HomePage;
